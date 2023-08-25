952136c660e5d5b9d99bc6504d43e2f234a50747
kepucino <okninskikarol2@gmail.com>
2023-08-25 19:57:39 +0200

    new file:   assets/scripts/plotly-locale-pl.js 	modified:   assets/scripts/script.js 	modified:   assets/styles/style.css 	modified:   index.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,16 @@
-const months = ["",
-  "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
-  "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
-];
+
 
 function checkParametersInput() {
-  // if (!power.value)
-    // nie ma podanej mocy instalacji
+  if (!markers.length) {
+    document.getElementById('map').style.border = "solid 1px red";
+    document.getElementById('map-wrong-input').style.display = "block";
+  }
 
-  // if (!markers.length)
-    // nie ma zadnego markera na mapie
+  if (!power.value) {
+    document.getElementById('form-input-parameters').classList.add('was-validated');
+  }
 
-  return (power.value && markers.length);
+  return (markers.length && power.value);
 }
 
 function checkEmailInput() {
@@ -26,57 +26,66 @@ function checkEmailInput() {
   return (input_email.value && checkbox_email.checked);
 }
 
-function displayPlot(plot_data) {
+function displayPlot(plot_data, id, cumulative=0) {
+  const cumulativeSumProduction = (sum => value => sum += value)(0);
+  const cumulativeSumProfit = (sum => value => sum += value)(0);
+
   var production = {
-    name: 'Produkcja w Wh',
+    name: (cumulative) ? 'Skumulowana produkcja (kWh)' : 'Produkcja (kWh)',
     x: plot_data.time,
-    y: plot_data.production,
+    y: (cumulative) ? plot_data.production.map(cumulativeSumProduction) : plot_data.production,
     type: 'scattergl',
-    fill: 'toself',
+    fill: (cumulative) ? 'tozeroy' : 'toself',
     stackgroup: 'one',
+    hovertemplate: (cumulative) ? 'Data: %{x}<br>Skumulowana produkcja: %{y} kWh<extra></extra>' : 'Data: %{x}<br>Produkcja: %{y} kWh<extra></extra>',
   };
   
   var profit = {
-    name: 'Zysk w PLN',
+    name: (cumulative) ? 'Skumulowany zysk (PLN)' : 'Zysk (PLN)',
     x: plot_data.time,
-    y: plot_data.profit,
+    y: (cumulative) ? plot_data.profit.map(cumulativeSumProfit) : plot_data.profit,
     type: 'scattergl',
-    fill: 'toself',
+    fill: (cumulative) ? 'tozeroy' : 'toself',
     stackgroup: 'one',
-    yaxis: 'y2'
+    hovertemplate: (cumulative) ? 'Data: %{x}<br>Skumulowany zysk: %{y} PLN<extra></extra>' : 'Data: %{x}<br>Zysk: %{y} PLN<extra></extra>',
+    yaxis: 'y2',
   };
   
   var data = [production, profit];
 
   var layout = {
-    // title: "Produkcja PV",
+    // title: {
+    //   text:(cumulative) ? 'Skumulowana produkcja i zysk' : 'Produkcja i zysk',
+    //   font: {
+    //     size: 24
+    //   },
+    //   x: 0,
+    // },
     margin: {
-      l: 65,
-      r: 40,
-      b: 10,
-      t: 30,
+      l: 60,
+      r: 60,
+      b: 20,
+      t: 20,
     },
     yaxis: {
-      title: 'Produkcja w kWh',
+      title: 'Produkcja (kWh)',
       titlefont: {
-        color: '#0022dd',
-        size: 18,
+        color: '#0A2FFF',
+        size: 21,
         weight: 'bold',
       },
-      // tickfont: {color: '#5599ff'},
     },
     yaxis2: {
-      title: 'Zysk w PLN',
+      title: 'Zysk (PLN)',
       titlefont: {
-        color: '#0066ff',
-        size: 18,
+        color: '#31D843',
+        size: 21,
         weight: 'bold',
       },
-      // tickfont: {color: '#0033FF'},
       overlaying: 'y',
       side: 'right'
     },
-    colorway: ['#0022dd', '#0066ff'],
+    colorway: ['#0A2FFF', '#31D843'],
     "showlegend": true,
     "legend": {
       "x": 0,
@@ -87,16 +96,22 @@ function displayPlot(plot_data) {
   var config = {
     responsive: true,
     displaylogo: false,
+    locale: "pl",
   };
 
-  Plotly.newPlot("plot", data, layout, config);
+  Plotly.newPlot(id, data, layout, config);
 }
 
 function displayTable(table_data) {
-  const summedProductionByMonth = [];
-  const summedProfitByMonth = [];
-  const summedProductionByYear = [];
-  const summedProfitByYear = [];
+  const polishMonths = ["",
+  "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
+  "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
+];
+
+  var summedProductionByMonth = [];
+  var summedProfitByMonth = [];
+  var summedProductionByYear = [];
+  var summedProfitByYear = [];
 
   for (let year = 2018; year <= 2050; year++) {
     summedProductionByMonth[year] = new Array(13).fill(0);
@@ -104,6 +119,11 @@ function displayTable(table_data) {
     summedProductionByYear[year] = 0;
     summedProfitByYear[year] = 0;
   }
+
+  // var allSummedProductionByMonth = new Array(13).fill(0);
+  // var allSummedProfitByMonth = new Array(13).fill(0);
+  var allSummedProduction = 0;
+  var allSummedProfit = 0;
 
   for (let i = 0; i < table_data.production.length; i++) {
     const production = table_data.production[i];
@@ -117,41 +137,100 @@ function displayTable(table_data) {
     summedProfitByMonth[year][month] += profit;
     summedProductionByYear[year] += production;
     summedProfitByYear[year] += profit;
+    // allSummedProductionByMonth[month] += production;
+    // allSummedProfitByMonth[month] += profit;
+    allSummedProduction += production;
+    allSummedProfit += profit;
   }
 
-  var prediction_table = document.getElementById("prediction-table");
+  var simulation_table = document.getElementById("simulation-table");
 
-  for (let year = 2022; year <= 2022; year++) {
+  while (simulation_table.firstChild) {
+    simulation_table.removeChild(simulation_table.firstChild);
+  }
+
+  var simulation_table_caption = document.createElement("caption");
+  simulation_table_caption.innerText = "Produkcja oraz zysk z instalacji fotowoltaiki";
+  simulation_table.appendChild(simulation_table_caption);
+
+  var simulation_table_head = document.createElement("thead");
+  simulation_table.appendChild(simulation_table_head);
+
+  let head_values = [
+    ["th", ""],
+    ["th", "Produkcja"],
+    ["th", "Zysk"]
+  ];
+
+  let row = document.createElement("tr");
+
+  for (let i=0; i < 3; i++) {
+    let c = document.createElement(head_values[i][0]);
+    c.innerText = head_values[i][1];
+    c.style.textAlign = "center";
+    row.appendChild(c);
+  }
+
+  simulation_table_head.appendChild(row);
+
+  var simulation_table_body = document.createElement("tbody");
+  simulation_table.appendChild(simulation_table_body);
+
+  var table_values = [];
+
+  for (let year = 2018; year <= 2023; year++) {
     for (let month = 1; month <= 12; month++) {
-      let row = document.createElement("tr")
-      let c1 = document.createElement("th")
-      let c2 = document.createElement("td")
-      let c3 = document.createElement("td")
+      let row_values = [
+        ["th", `${year} ${polishMonths[month]}`],
+        ["td", `${summedProductionByMonth[year][month].toFixed(2)} kWh`],
+        ["td", `${summedProfitByMonth[year][month].toFixed(2)} PLN`]
+      ];
 
-      c1.innerText = months[month]
-      c2.innerText = (summedProductionByMonth[year][month] / 1000).toFixed(2);
-      c3.innerText = summedProfitByMonth[year][month].toFixed(2);
-      
-      row.appendChild(c1);
-      row.appendChild(c2);
-      row.appendChild(c3);
-      
-      prediction_table.appendChild(row)
+      table_values.push(row_values);
     }
-    let row = document.createElement("tr")
-    let c1 = document.createElement("th")
-    let c2 = document.createElement("td")
-    let c3 = document.createElement("td")
 
-    c1.innerText = ''
-    c2.innerText = (summedProductionByYear[year] / 1000).toFixed(2);
-    c3.innerText = summedProfitByYear[year].toFixed(2);
+    let row_sum = [
+      ["th", "Suma z całego roku"],
+      ["td", `${summedProductionByYear[year].toFixed(2)} kWh`],
+      ["td", `${summedProfitByYear[year].toFixed(2)} PLN`]
+    ];
+  
+    table_values.push(row_sum);
+
+    let row_space = [
+      ["th", " "],
+      ["td", " "],
+      ["td", " "]
+    ];
+  
+    table_values.push(row_space);
+  }
+
+  let row_all_sum = [
+    ["th", "Suma ze wszystkich lat"],
+    ["td", `${allSummedProduction.toFixed(2)} kWh`],
+    ["td", `${allSummedProfit.toFixed(2)} PLN`]
+  ];
+
+  table_values.push(row_all_sum);
+
+  console.log(table_values)
+
+  for (let i=0; i < table_values.length; i++) {
+    let row = document.createElement("tr");
+    row.style.height = "2em";
+
+    for (let j=0; j < 3; j++) {
+      let c = document.createElement(table_values[i][j][0]);
+      c.innerText = table_values[i][j][1];
+
+      if (j > 0)
+        c.style.textAlign = "center";
+
+      row.appendChild(c);
+    }
     
-    row.appendChild(c1);
-    row.appendChild(c2);
-    row.appendChild(c3);
-    
-    prediction_table.appendChild(row)
+    simulation_table_body.appendChild(row);
   }
 }
 
@@ -200,7 +279,7 @@ function displayTableOnSlider(table_data) {
       let row = document.createElement("div");
       row.setAttribute("class", "row");
 
-      let cols_values = [months[month], (summedProductionByMonth[year][month] / 1000).toFixed(2), summedProfitByMonth[year][month].toFixed(2)];
+      let cols_values = [polishMonths[month], (summedProductionByMonth[year][month] / 1000).toFixed(2), summedProfitByMonth[year][month].toFixed(2)];
 
       if (month == 0) // table labels
         cols_values = ["", "Produkcja w kWh", "Zysk w PLN"];
@@ -240,16 +319,22 @@ function addNewMarker(geolocation) {
   map.addLayer(marker);
   markers.push(marker);
 
+  if (document.getElementById('map-wrong-input').style.display == "block") // prevent green border without previous error
+    document.getElementById('map').style.border = "solid 2px green";
+
+  document.getElementById('map-wrong-input').style.display = "none";
+
   document.getElementById("map-lat").innerText = 'Szerokość: ' + geolocation.lat
   document.getElementById("map-lng").innerText = 'Wysokość: ' + geolocation.lng
 }
 
 var power = document.getElementById("input-power");
 var angle = document.getElementById("input-angle");
-var azimuth = document.getElementById("input-azimuth");
+// var azimuth = document.getElementById("input-azimuth");
+var azimuth = document.getElementById("input-select-azimuth");
 var button_calculate = document.getElementById("button-calculate");
-var prediction_card = document.getElementById("prediction-card");
-var span_wrong_input = document.getElementById("span-wrong-input");
+var simulation_card = document.getElementById("simulation-card");
+// var span_wrong_input = document.getElementById("span-wrong-input");
 
 // power.oninput = function() {
 //   power.value = this.value;
@@ -257,17 +342,17 @@ var span_wrong_input = document.getElementById("span-wrong-input");
 
 angle.oninput = function() {
   angle.value = this.value;
-  document.getElementById("label-angle").innerText = `Kąt nachylenia: ${this.value}°`;
+  document.getElementById("label-angle").innerText = `Kąt nachylenia paneli: ${this.value}°`;
 }
 
 azimuth.oninput = function() {
-  azimuth.value = this.value;
-  document.getElementById("label-azimuth").innerText = `Azymut: ${this.value}°`;
+  console.log(azimuth.options[azimuth.selectedIndex].text)
+  azimuth.options[azimuth.selectedIndex].innerText = `Kierunek paneli: ${azimuth.options[azimuth.selectedIndex].text}`;
 }
 
 // map
 
-const markers = [];
+var markers = [];
 var map = L.map('map').setView([52, 19], 6);
 
 L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -284,12 +369,14 @@ map.on('click', function(e) {
   addNewMarker(e.latlng);
 });
 
-// show prediction card
+// show simulation card
 
 button_calculate.onclick = function() {
   if (checkParametersInput()) {
-    prediction_card.style.display = "block";
-    span_wrong_input.style.display = "none";
+    simulation_card.style.display = "block";
+    document.getElementById("span-wrong-input").style.display = "none";
+
+    console.log(parseInt(power.value))
 
     const url = 'http://api-rce.azurewebsites.net:80/api/estimate';
     const request_data = {
@@ -299,11 +386,11 @@ button_calculate.onclick = function() {
         lon: markers[0].lng,
         azimuth: parseInt(azimuth.value),
         angle: parseInt(angle.value),
-        peak_power: parseInt(power.value) * 1000,
+        peak_power: parseFloat(power.value) * 1000, // convert from kWp to Wp
     };
     
     const headers = new Headers();
-    headers.append('Authorization', 'Bearer API_key_here');
+    headers.append('Authorization', 'Bearer f14ab0c5-b4b9-4674-8739-6d3319f94490');
     headers.append('Content-Type', 'application/json');
     
     fetch(url, {
@@ -314,9 +401,14 @@ button_calculate.onclick = function() {
     .then(response => response.json())
     .then(response_data => {
         console.log(response_data);
+
+        for (let i=0; i < response_data.production.length; i++)
+        response_data.production[i] /= 1000 // convert from Wp to kWp
     
-        displayPlot(plot_data=response_data);
-        displayTableOnSlider(table_data=response_data);
+        displayPlot(plot_data=response_data, id='plot');
+        displayPlot(plot_data=response_data, id='plot-cumulative', cumulative=1);
+        // displayTableOnSlider(table_data=response_data);
+        displayTable(table_data=response_data);
     })
     .catch(error => {
         console.error('Error:', error);
@@ -325,7 +417,7 @@ button_calculate.onclick = function() {
 
   else {
     console.log('zle podane dane');
-    span_wrong_input.style.display = "block";
+    document.getElementById("span-wrong-input").style.display = "block";
   }
 }
 
